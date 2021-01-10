46ccb66f1a162c351c38fb557b4ecde45f20e51c
Jieun Jeon <jieunjeon1023@gmail.com>
2021-01-10 00:56:15 -0800

    add useFadeIn css styles

---
+++ useFadeIn/useFadeIn.js
@@ -0,0 +1,15 @@
+export const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element, current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s easy`
+      current.style.opapcity = 1;
+    }
+  }, []);
+  return { ref: element, style { opacity: 0 }
+}
+}
